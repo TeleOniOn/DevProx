828c239a282783efabddb0f77783b27438fd66e7
LOST2014 <80536220+LOST2014@users.noreply.github.com>
2021-03-22 17:19:55 -0700

    Update DevProx.sh

--- DevProx.sh
+++ DevProx.sh
@@ -3,7 +3,7 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 Version=`lsb_release -rs | cut -f1 -d"."`
 install() {
 echo -e "\033[0;36m"
-echo -e "~ Welcome To Source DevProx - اهلا بك في سورس ديف بروكس ~"
+echo -e "~ Welcome To Source AOmDa - اهلا بك في سورس العمده ~"
 echo -e "     ~ { Setting Up Offices - جاري رفع المكاتب } ~"
 echo -e "\033[0m"
 sudo apt-get update -y
@@ -43,16 +43,10 @@ sudo luarocks install luautf8
 cd .. ;sudo rm -rf luarocks*
 sudo timedatectl set-timezone Asia/Baghdad
 echo -e "\033[0;36m"
-echo -e "---------------------------------------------"
-echo -e "|    ____             ____                  |"
-echo -e "|   |  _ \  _____   _|  _ \ _ __ _____  __  |"
-echo -e "|   | | | |/ _ \ \ / / |_) | '__/ _ \ \/ /  |"
-echo -e "|   | |_| |  __/\ V /|  __/| | | (_) >  <   |"
-echo -e "|   |____/ \___| \_/ |_|   |_|  \___/_/\_\  |"
 echo -e "|-------------------------------------------|"
-echo -e "|This Source Was Developed By (ABS) @IQ_ABS.|"
-echo -e "|  This Is The Source Channel @Dev_Prox .   |"
-echo -e "|               - DevProx -                 |"
+echo -e "|This Source Was  Developed By (MeM) @IVEN4.|"
+echo -e "|  This Is The Source  Channel @Src_Web .   |"
+echo -e "|                - AlomDa -                 |"
 echo -e "---------------------------------------------"
 echo -e "\033[0m"
 cd $THIS_DIR && sudo chmod +x tg
